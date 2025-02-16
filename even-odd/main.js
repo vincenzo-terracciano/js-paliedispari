dfbd67a3fd701614790013cab032dc158a311ee8
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-16 13:01:04 +0100

    add even or odd exercise

--- even-odd/main.js
+++ even-odd/main.js
@@ -8,5 +8,34 @@ console.log(userChoice);
 const userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
 console.log(userNumber);
 
-const pcNumber = Math.ceil(Math.random() * 5);
-console.log(pcNumber);
+
+function randomNumber () {
+    const pcNumber = Math.ceil(Math.random() * 5);
+    return pcNumber;
+    
+}
+
+const pcChoice = randomNumber();
+console.log(pcChoice);
+
+function isSumEvenOdd(user, pc) {
+    const sum = user + pc;
+    console.log(sum);
+    if (sum % 2 === 0) {
+        return `${sum} is even number`
+    } else {
+        return `${sum} is odd number`
+    }
+}
+
+const answer = isSumEvenOdd(userNumber, pcChoice);
+console.log(answer);
+
+
+
+
+
+
+
+
+
